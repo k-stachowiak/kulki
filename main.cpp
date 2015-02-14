c8239db75015edb7497891b48917505b272b22cb
k-stachowiak <krzysiek.stachowiak@gmail.com>
2015-02-14 22:15:10 +0100

    Very basic gameplay in place.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
-#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <algorithm>
 #include <iostream>
+#include <limits>
+#include <vector>
+#include <random>
+#include <deque>
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
@@ -9,7 +16,18 @@
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_primitives.h>
 
-class allegro {
+#include <glm/glm.hpp>
+
+namespace std {
+    template <> struct hash<std::pair<int, int>> {
+        size_t operator()(const std::pair<int, int> &v) const {
+            std::hash<int> int_hasher;
+            return int_hasher(v.first) ^ int_hasher(v.second);
+        }
+    };
+}
+
+class Allegro {
 
     const int m_screen_w, m_screen_h;
     const double m_fps, m_spf;
@@ -18,13 +36,15 @@ class allegro {
     ALLEGRO_DISPLAY* m_display;
     ALLEGRO_EVENT_QUEUE* m_ev_queue;
 
-    std::vector<bool> m_keys;
+    Allegro(const Allegro&);    // Not copyable ...
+    Allegro(Allegro&&);         // ... or moveable.
 
-    allegro(const allegro&);    // Not copyable ...
-    allegro(allegro&&);         // ... or moveable.
-
-    template <typename KeyEvent>
-    void m_process_events(KeyEvent key, bool& alive)
+    template <typename KeyEvent, typename ButtonEvent, typename CursorEvent>
+    void m_process_events(
+            KeyEvent key,
+            ButtonEvent button,
+            CursorEvent cursor,
+            bool& alive)
     {
         ALLEGRO_EVENT event;
 
@@ -36,13 +56,23 @@ class allegro {
                 return;
 
             case ALLEGRO_EVENT_KEY_DOWN:
-                m_keys[event.keyboard.keycode] = true;
                 key(event.keyboard.keycode, true);
                 break;
 
             case ALLEGRO_EVENT_KEY_UP:
-                m_keys[event.keyboard.keycode] = false;
                 key(event.keyboard.keycode, false);
+                break;
+
+            case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+                button(event.mouse.button, true);
+                break;
+
+            case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+                button(event.mouse.button, false);
+                break;
+
+            case ALLEGRO_EVENT_MOUSE_AXES:
+                cursor(event.mouse.x, event.mouse.y);
                 break;
 
             default:
@@ -77,11 +107,10 @@ class allegro {
     }
 
 public:
-    allegro() :
-        m_screen_w { 800 }, m_screen_h { 600 },
+    Allegro() :
+        m_screen_w { 800 }, m_screen_h { 800 },
         m_fps { 50.0 }, m_spf { 1.0 / m_fps },
-        m_display { nullptr }, m_ev_queue { nullptr },
-        m_keys(ALLEGRO_KEY_MAX, false)
+        m_display { nullptr }, m_ev_queue { nullptr }
     {
         if (!al_init()) {
             std::cerr << "Failed initializing platform wrapper." << std::endl;
@@ -120,6 +149,11 @@ public:
             exit(1);
         }
 
+        if (!al_install_mouse()) {
+            std::cerr << "Failed installing mouse." << std::endl;
+            exit(1);
+        }
+
         if(!al_install_audio()) {
             std::cerr << "Failed initializing audio." << std::endl;
             exit(1);
@@ -133,22 +167,34 @@ public:
 
         al_register_event_source(m_ev_queue, al_get_display_event_source(m_display));
         al_register_event_source(m_ev_queue, al_get_keyboard_event_source());
+        al_register_event_source(m_ev_queue, al_get_mouse_event_source());
     }
 
-    ~allegro()
+    ~Allegro()
     {
         al_destroy_event_queue(m_ev_queue);
         al_destroy_display(m_display);
     }
 
-    template <typename KeyEvent, typename TickEvent, typename DrawEvent>
-    void realtime_loop(KeyEvent key, TickEvent tick, DrawEvent draw, bool& alive)
+    template <
+        typename KeyEvent,
+        typename ButtonEvent,
+        typename CursorEvent,
+        typename TickEvent,
+        typename DrawEvent>
+    void realtime_loop(
+            KeyEvent key,
+            ButtonEvent button,
+            CursorEvent cursor,
+            TickEvent tick,
+            DrawEvent draw,
+            bool& alive)
     {
         m_current_time = al_get_time();
         m_accumulator = 0;
 
         while (true) {
-            m_process_events(key, alive);
+            m_process_events(key, button, cursor, alive);
             if (!alive) return;
 
             m_realtime_loop_step(tick, draw, alive);
@@ -160,28 +206,499 @@ public:
     
 };
 
-bool alive = true;
+const int EMPTY = -1;
 
-void on_key(int key, bool down)
-{
-    if (key == ALLEGRO_KEY_ESCAPE && down)
+struct Board {
+
+    int m_width, m_height;
+    std::vector<int> m_fields;
+
+    // Semi-regular:
+    Board() : m_width { 0 }, m_height { 0 } {};
+    Board(const Board& x) : m_width { x.m_width }, m_height { x.m_height }, m_fields { x.m_fields } {}
+    Board(Board&& x) : m_width { x.m_width }, m_height { x.m_height }, m_fields { std::move(x.m_fields) } {}
+    Board& operator=(const Board& x)
     {
-        alive = false;
+        m_width = x.m_width;
+        m_height = x.m_height;
+        m_fields = x.m_fields;
+        return *this;
+    }
+    Board& operator=(Board&& x)
+    {
+        m_width = x.m_width;
+        m_height = x.m_height;
+        m_fields = std::move(x.m_fields);
+        return *this;
+    }
+
+    // Custom constructor:
+    Board(int w, int h) :
+        m_width { w }, m_height { h },
+        m_fields(w * h, EMPTY)
+    {}
+
+    // API:
+    void clear()
+    {
+        std::fill(begin(m_fields), end(m_fields), EMPTY);
+    }
+
+    bool has(int x, int y) const
+    {
+        return x >= 0 && x < m_width && y >= 0 && y < m_height;
+    }
+
+    int &operator()(int x, int y)
+    {
+        return m_fields[y * m_width + x];
+    }
+
+    int operator()(int x, int y) const
+    {
+        return const_cast<Board&>(*this)(x, y);
+    }
+};
+
+int free_fields(const Board& b)
+{
+    return std::count(begin(b.m_fields), end(b.m_fields), EMPTY);
+}
+
+bool find_path(
+        const Board& b,
+        std::pair<int, int> src, std::pair<int, int> dst,
+        std::deque<std::pair<int, int>>& path)
+{
+    std::unordered_set<std::pair<int, int>> open;
+    std::unordered_map<std::pair<int, int>, std::pair<int, int>> preds;
+    std::unordered_map<std::pair<int, int>, int> dists;
+
+    for (int x = 0; x < b.m_width; ++x) {
+        for (int y = 0; y < b.m_height; ++y) {
+            dists[std::pair<int, int>(x, y)] = std::numeric_limits<int>::max();
+        }
+    }
+
+    open.insert(src);
+    dists[src] = 0;
+
+    while (!open.empty()) {
+
+        auto it = std::min_element(begin(open), end(open),
+            [&dists](const std::pair<int, int>& x, const std::pair<int, int>& y) {
+                return dists[x] < dists[y];
+            });
+
+        std::pair<int, int> u = *it;
+        if (u == dst) {
+            break;
+        }
+
+        std::vector<std::pair<int, int>> neighbors {
+            { u.first - 1, u.second },
+            { u.first + 1, u.second },
+            { u.first, u.second - 1 },
+            { u.first, u.second + 1 },
+        };
+
+        int new_dist = dists[u] + 1;
+        for (const auto& v : neighbors) {
+            if (b(v.first, v.second) != EMPTY) {
+                continue;
+            }
+            if (new_dist < dists[v]) {
+                dists[v] = new_dist;
+                preds[v] = u;
+                open.insert(v);
+            }
+        }
+
+        open.erase(u);
+    }
+
+    if (dists[dst] == std::numeric_limits<int>::max()) {
+        return false;
+    } else {
+        std::pair<int, int> n = dst;
+        while (n != src) {
+            path.push_front(n);
+            n = preds[n];
+        }
+        path.push_front(n);
+        return true;
     }
 }
 
-void on_tick(double)
+glm::mat3 translate(double x, double y)
 {
+    return {
+        1, 0, x,
+        0, 1, y,
+        0, 0, 1
+    };
 }
 
-void on_draw(double)
+glm::mat3 scale(double factor)
 {
-    al_clear_to_color(al_map_rgb_f(0.5, 0.5, 0.5));
-    al_flip_display();
+    return {
+        factor, 0, 0,
+        0, factor, 0,
+        0, 0, 1
+    };
 }
+
+/// Draw point.
+void draw(const glm::vec3& p, const glm::mat3& transf)
+{
+    glm::vec3 out_p = p * transf;
+    al_draw_pixel(out_p.x, out_p.y, al_map_rgb_f(1, 1, 1));
+}
+
+/// Draw rectangle.
+void draw(
+        const glm::vec3& top_left,
+        const glm::vec3& bot_right,
+        const glm::mat3& transf)
+{
+    glm::vec3 out_top_left = top_left * transf;
+    glm::vec3 out_bot_right = bot_right * transf;
+    double x1 = out_top_left.x, y1 = out_top_left.y;
+    double x2 = out_bot_right.x, y2 = out_bot_right.y;
+    al_draw_rectangle(x1, y1, x2, y2, al_map_rgb_f(1, 1, 0.5), 1.0);
+}
+
+/// Draw ball.
+void draw(double x, double y, int color, double r, bool fill, const glm::mat3& transf)
+{
+    static std::array<ALLEGRO_COLOR, 8> ball_colors {
+        al_map_rgb_f(0, 0, 0),
+        al_map_rgb_f(0, 0, 1),
+        al_map_rgb_f(0, 1, 0),
+        al_map_rgb_f(0, 1, 1),
+        al_map_rgb_f(1, 0, 0),
+        al_map_rgb_f(1, 0, 1),
+        al_map_rgb_f(1, 1, 0),
+        al_map_rgb_f(1, 1, 1)
+    };
+
+    glm::vec3 c = glm::vec3 { x, y, 1 } * transf;
+
+    if (fill) {
+        al_draw_filled_circle(c.x, c.y, r, ball_colors[color]);
+    } else {
+        al_draw_circle(c.x, c.y, r, ball_colors[color], 3);
+    }
+}
+
+void draw(const Board& b, int hlx, int hly, const glm::mat3& transf)
+{
+    for (int x = 0; x < b.m_width; ++x) {
+        for (int y = 0; y < b.m_height; ++y) {
+            if (x != hlx || y != hly) {
+                draw(glm::vec3 { x + 0.05, y + 0.05, 1.0 }, glm::vec3 { x + 0.95, y + 0.95, 1.0 }, transf);
+            }
+            if (b(x, y) != EMPTY) {
+                draw(x + 0.5, y + 0.5, b(x, y), 25, true, transf);
+            }
+        }
+    }
+}
+
+enum KulkiState {
+    DEAL,
+    WAIT_BALL,
+    WAIT_DEST,
+    MOVE,
+    GAMEOVER
+};
+
+class Kulki {
+
+    std::default_random_engine m_reng;
+
+    bool m_alive;
+    Board m_board;
+    ALLEGRO_FONT* m_font;
+
+    std::pair<int, int> m_cursor_screen;
+    std::pair<int, int> m_cursor_tile;
+
+    KulkiState m_state;
+
+    int m_deal_count;
+    double m_deal_time;
+
+    int m_waitd_src_x, m_waitd_src_y;
+    int m_waitd_color;
+
+    std::deque<std::pair<int, int>> m_move_path;
+    double m_move_time;
+    int m_move_dst_x, m_move_dst_y;
+    int m_move_color;
+
+    Kulki(const Kulki&);    // Not copyable ...
+    Kulki(Kulki&&);         // ... or moveable.
+
+    // Helpers.
+    // --------
+
+    glm::mat3 m_current_transform()
+    {
+        return scale(64.0) * translate(20, 40); 
+    }
+
+    void m_new_ball(int& x, int& y, int& color)
+    {
+        std::uniform_int_distribution<int> distr_x(0, m_board.m_width - 1);
+        std::uniform_int_distribution<int> distr_y(0, m_board.m_height - 1);
+        std::uniform_int_distribution<int> distr_color(0, 7);
+
+        do {
+            x = distr_x(m_reng);
+            y = distr_y(m_reng);
+        } while (m_board(x, y) != EMPTY);
+
+        color = distr_color(m_reng);
+    }
+
+    // State transitions.
+    // ------------------
+
+    void m_set_state_wait_ball()
+    {
+        m_state = WAIT_BALL;
+    }
+
+    void m_reset_state_wait_ball(int src_x, int src_y, int color)
+    {
+        m_board(src_x, src_y) = color;
+        m_state = WAIT_BALL;
+    }
+
+    void m_set_state_wait_dest(int src_x, int src_y)
+    {
+        m_waitd_src_x = src_x;
+        m_waitd_src_y = src_y;
+        m_waitd_color = m_board(src_x, src_y);
+        m_board(src_x, src_y) = EMPTY;
+        m_state = WAIT_DEST;
+    }
+
+    void m_set_state_deal(int count)
+    {
+        if (free_fields(m_board) <= count) {
+            m_set_state_gameover();
+            return;
+        }
+
+        m_deal_count = count;
+        m_deal_time = 0.25;
+        m_state = DEAL;
+    }
+
+    void m_set_state_gameover()
+    {
+        m_state = GAMEOVER;
+    }
+
+    void m_set_state_move(int src_x, int src_y, int dst_x, int dst_y, int color)
+    {
+        std::deque<std::pair<int, int>> path;
+
+        if (!find_path(m_board, { src_x, src_y }, { dst_x, dst_y }, path)) {
+            m_reset_state_wait_ball(src_x, src_y, color);
+            return;
+        }
+
+        m_move_path = std::move(path);
+        m_move_time = 0.25;
+        m_move_dst_x = dst_x;
+        m_move_dst_y = dst_y;
+        m_move_color = color;
+        m_state = MOVE;
+    }
+
+    // Tick implementations.
+    // ---------------------
+
+    void m_tick_deal(double dt)
+    {
+        if ((m_deal_time -= dt) > 0) return;
+        else m_deal_time = 0.25;
+
+        int x, y, color;
+        m_new_ball(x, y, color);
+        m_board(x, y) = color;
+
+        if ((--m_deal_count) == 0) {
+            m_set_state_wait_ball();
+        }
+    }
+
+    void m_tick_move(double dt)
+    {
+        if ((m_move_time -= dt) > 0) return;
+        else m_move_time = 0.25;
+
+        m_move_path.pop_front();
+
+        if (m_move_path.empty()) {
+            m_board(m_move_dst_x, m_move_dst_y) = m_move_color;
+            m_set_state_deal(3);
+        }
+    }
+
+    // Draw implementations.
+    // ---------------------
+
+    void m_draw_wait_dest()
+    {
+        glm::mat3 transf = m_current_transform();
+        int x = m_waitd_src_x;
+        int y = m_waitd_src_y;
+        draw(x + 0.5, y + 0.5, m_waitd_color, 25, false, transf);
+    }
+
+    void m_draw_move()
+    {
+        glm::mat3 transf = m_current_transform();
+        int x = m_move_path.front().first;
+        int y = m_move_path.front().second;
+        draw(x + 0.5, y + 0.5, m_move_color, 25, false, transf);
+    }
+
+    void m_draw_gameover()
+    {
+        al_draw_text(m_font, al_map_rgb_f(1, 0, 0), 400, 400, ALLEGRO_ALIGN_CENTRE, "Game Over");
+    }
+
+    // Common implementations.
+    // -----------------------
+
+    void m_on_key(int key, bool down)
+    {
+        if (key == ALLEGRO_KEY_ESCAPE && down)
+        {
+            m_alive = false;
+            return;
+        }
+
+        switch (m_state)
+        {
+        case GAMEOVER:
+            m_board.clear();
+            m_set_state_deal(7);
+            break;
+        default:
+            break;
+        }
+    }
+
+    void m_on_button(int button, bool down)
+    {
+        if (!down) return;
+
+        int tx = m_cursor_tile.first;
+        int ty = m_cursor_tile.second;
+
+        switch (m_state) {
+        case WAIT_BALL:
+            if (m_board.has(tx, ty)) {
+                m_set_state_wait_dest(tx, ty);
+            }
+            break;
+        case WAIT_DEST:
+            if (tx == m_waitd_src_x && ty == m_waitd_src_y) {
+                m_reset_state_wait_ball(m_waitd_src_x, m_waitd_src_y, m_waitd_color);
+            } else {
+                m_set_state_move(m_waitd_src_x, m_waitd_src_y, tx, ty, m_waitd_color);
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+    void m_on_cursor(int x, int y)
+    {
+        m_cursor_screen.first = x;
+        m_cursor_screen.second = y;
+
+        glm::mat3 inv = glm::inverse(m_current_transform());
+        glm::vec3 screen_pos { x, y, 1 };
+        glm::vec3 tile_pos = screen_pos * inv;
+
+        m_cursor_tile.first = tile_pos.x;
+        m_cursor_tile.second = tile_pos.y;
+    }
+
+    void m_tick(double dt)
+    {
+        switch (m_state) {
+        case DEAL:
+            m_tick_deal(dt);
+            break;
+        case MOVE:
+            m_tick_move(dt);
+            break;
+        default:
+            break;
+        }
+    }
+
+    void m_draw(double)
+    {
+        glm::mat3 transf = m_current_transform();
+        al_clear_to_color(al_map_rgb_f(0.5, 0.5, 0.5));
+        draw(m_board, m_cursor_tile.first, m_cursor_tile.second, transf);
+        switch (m_state)
+        {
+        case WAIT_DEST:
+            m_draw_wait_dest();
+            break;
+        case MOVE:
+            m_draw_move();
+            break;
+        case GAMEOVER:
+            m_draw_gameover();
+            break;
+        default:
+            break;
+        }
+        al_flip_display();
+    }
+
+public:
+    Kulki() :
+        m_alive { true },
+        m_board { 5, 5 },
+        m_font { al_load_font("prstartk.ttf", -64, 0) }
+    {
+        m_set_state_deal(7);
+    }
+
+    ~Kulki()
+    {
+        al_destroy_font(m_font);
+    }
+
+    void run(Allegro& al)
+    {
+        using namespace std::placeholders;
+        al.realtime_loop(
+            std::bind(&Kulki::m_on_key, this, _1, _2),
+            std::bind(&Kulki::m_on_button, this, _1, _2),
+            std::bind(&Kulki::m_on_cursor, this, _1, _2),
+            std::bind(&Kulki::m_tick, this, _1),
+            std::bind(&Kulki::m_draw, this, _1),
+            m_alive);
+    }
+};
 
 int main()
 {
-    allegro al;
-    al.realtime_loop(on_key, on_tick, on_draw, alive);
+    Allegro al;
+    (Kulki {}).run(al);
 }
+

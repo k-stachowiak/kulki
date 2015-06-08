8ccf1bb9f137104f614c61420d6e00eddf842ccd
k-stachowiak <krzysiek.stachowiak@gmail.com>
2015-06-08 23:02:04 +0200

    Added ball spawning animation

--- states/deal_state.cpp
+++ states/deal_state.cpp
@@ -6,21 +6,8 @@ namespace {
     std::mt19937 engine { device() };
 }
 
-DealState::DealState(KulkiContext* context) :
-    m_context { context }
-{}
-
-void DealState::reset(double time)
+void DealState::deal_next()
 {
-    m_time = time;
-    m_positions.clear();
-}
-
-void DealState::tick(double dt)
-{
-    if ((m_time -= dt) > 0) return;
-    else m_time = config::DEAL_PERIOD;
-
     std::uniform_int_distribution<int> distr_x(0, m_context->m_board->m_width - 1);
     std::uniform_int_distribution<int> distr_y(0, m_context->m_board->m_height - 1);
 
@@ -33,12 +20,44 @@ void DealState::tick(double dt)
         new_y = distr_y(engine);
     } while ((*m_context->m_board)(new_x, new_y) != config::EMPTY);
 
-    (*m_context->m_board)(new_x, new_y) = new_color;
-    m_positions.emplace_back(new_x, new_y);
+    m_current_x = new_x;
+    m_current_y = new_y;
+    m_current_color = new_color;
+}
+
+DealState::DealState(KulkiContext* context) :
+    m_context { context }
+{}
+
+void DealState::reset(double time)
+{
+    m_time = time;
+    m_positions.clear();
+
+    deal_next();
+}
+
+void DealState::tick(double dt)
+{
+    if ((m_time -= dt) > 0) return;
+    else m_time = config::DEAL_PERIOD;
+
+    (*m_context->m_board)(m_current_x, m_current_y) = m_current_color;
+    m_positions.emplace_back(m_current_x, m_current_y);
 
     if (m_context->m_next_deal.empty()) {
         m_context->gen_next_deal(config::DEAL_COUNT_INGAME);
         m_context->set_state_score(m_positions, KulkiState::Enum::WAIT_BALL);
+    } else {
+        deal_next();
     }
+}
+
+void DealState::draw(const glm::mat3& transf)
+{
+    double squeeze = 1.0 - double(m_time) / config::DEAL_PERIOD;
+    double x = double(m_current_x) + 0.5;
+    double y = double(m_current_y) + 0.5;
+    m_context->draw_ball(x, y, m_current_color, config::BALL_RADIUS, squeeze, transf);
 }
 
--- states/deal_state.h
+++ states/deal_state.h
@@ -7,13 +7,17 @@ class DealState : public KulkiState {
 
     KulkiContext* const m_context;
 
+    int m_current_x, m_current_y, m_current_color;
     double m_time;
     std::vector<std::pair<int, int>> m_positions;
+
+    void deal_next();
 
 public:
     DealState(KulkiContext* context);
     void reset(double time);
     void tick(double dt) override;
+    void draw(const glm::mat3& transf) override;
 };
 
 #endif
